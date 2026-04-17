4024b6a19f4ddf71816ace29a73d576bd825e4d3
Imre Jakobcsics <jakobcsicsimi@gmail.com>
2026-04-17 14:06:35 +0200

    chore: setup Dockerfile, docker compose

--- src/config/env.schema.ts
+++ src/config/env.schema.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   PORT: z.coerce.number(),
+  DATABASE_CONNECTION_STRING: z.string().min(1),
 });
 
 export type Env = z.infer<typeof envSchema>;
